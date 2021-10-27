e47331824851dcba87d8152f2372af779d55895d
appsierraqa <keshav.murthy@appsierra.com>
2021-10-27 21:04:15 +0530

    updated wait

--- src/main/java/commons/BasePage.java
+++ src/main/java/commons/BasePage.java
@@ -267,6 +267,6 @@ public abstract class BasePage {
 
 		wait.forElementToBeVisible(myViewsDropDown);
 		dropDownMethod(myViewsDropDown, "VisibleText", "Default View");
-		wait.forPage();
+		wait.forPage(5000);
 	}
 }
